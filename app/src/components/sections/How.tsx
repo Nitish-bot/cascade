c716f0adf03b7e4a463b7ef63872d7b4c6471a4d
Nitish C <nitishc.work@gmail.com>
2025-09-17 18:04:55 +0530

    Skip the how section for now

--- app/src/components/sections/How.tsx
+++ app/src/components/sections/How.tsx
@@ -1,11 +1,9 @@
-import Star21 from "../assets/star-21";
-import Star8 from "../assets/star-8";
-import Star9 from "../assets/star-9";
+import Star38 from "@/components/assets/star-38";
 
 function How() {
   return (
     <div className="min-h-[100vh] w-full max-w-lg md:max-w-2xl lg:max-w-3xl xl:max-w-5xl mx-auto">
-      <Star8 />
+      <Star38 />
     </div>
 )};
 
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -12,7 +12,7 @@ function Home() {
       <Hero />
       <Why />
       <How />
-      <FAQs />
+      {/* <FAQs /> */}
       <Footer />
     </>
   );
